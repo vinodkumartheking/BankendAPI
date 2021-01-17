fe188c7f8cf92a3bbc161a7734797242df94d40f
unknown <vinodkumartheking@gmail.com>
2021-01-17 16:19:05 +0530

    added code for search and cros

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
+const cors = require('cors');
 dotenv.config()
 
-mongoose.connect("mongodb+srv://dbadminusr:dbadmin@cluster0.h8q7y.mongodb.net/DoctorDB?retryWrites=true&w=majority")
+mongoose.connect(process.env.MongoDBfullurl)
 var app = express()
 
+app.use(cors());
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
@@ -16,7 +18,7 @@ app.get("/",(req,res)=>{
 })
 
 var MongoClient = require("mongodb").MongoClient;
-let url = "mongodb+srv://dbadminusr:dbadmin@cluster0.h8q7y.mongodb.net/"
+let url = process.env.MongoDBcollectionURL //"mongodb+srv://dbadminusr:dbadmin@cluster0.h8q7y.mongodb.net/"
 
 app.get("/getdoctor",(req,res)=>{
     MongoClient.connect(url,(err,db)=>{
@@ -30,6 +32,27 @@ app.get("/getdoctor",(req,res)=>{
             db.close()
         })
     })
+})
+
+app.get("/getdoctor/id",(req,res)=>{
+    MongoClient.connect(url,(err,db)=>{
+        
+        let dbo = db.db("DoctorDB")
+        let query = {Id:ObjectId(`${id}`)}
+        dbo.collection("DoctorCollection").find(query).toArray((err,result)=>{
+            if(err) throw err;
+            console.log(result)
+            res.send(result)
+            db.close()
+        })
+    })
+})
+
+app.post('/bookappointment',(req,res)=>{
+    req.headers("Auth","slkdjf342knd")
+    //code to save in mongobd
+    res.send()
+    db.close()
 })
 
 app.listen(process.env.PORT || 3000,(req,res)=>{
